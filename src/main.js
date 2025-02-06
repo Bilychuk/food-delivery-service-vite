024a6227e247c163e0cd6fc84f1750a19f87f5dd
Bilychuk Kateryna <bilychukkateryna@ukr.net>
2025-02-06 15:57:11 +0200

    add modal windows

--- src/main.js
+++ src/main.js
@@ -4,12 +4,29 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
+const menuOpenBtn = document.querySelector('.burger-menu');
+const menuCloseBtn = document.querySelector('.mobile-menu-close-btn');
+const mobileMenu = document.querySelector('.mobile-menu');
+const orderWindow = document.querySelector('.backdrop');
+const orderOpenBtns = document.querySelectorAll('.order-open-btn');
+const orderCloseBtn = document.querySelector('.order-modal-close-btn');
+
+const onToggleMenuHandler = () => mobileMenu.classList.toggle('is-open');
+
+menuOpenBtn.addEventListener('click', onToggleMenuHandler);
+menuCloseBtn.addEventListener('click', onToggleMenuHandler);
+
+const onToggleOrderHandler = () => orderWindow.classList.toggle('is-open');
+
+orderOpenBtns.forEach(btn =>
+  btn.addEventListener('click', onToggleOrderHandler)
+);
+orderCloseBtn.addEventListener('click', onToggleOrderHandler);
+
 const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
   loop: true,
   modules: [Navigation],
-
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
